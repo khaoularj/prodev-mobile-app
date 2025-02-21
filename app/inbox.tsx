20d2da26ca5e1b413e866619a995df5d2343a44a
khaoularj <khaoula.raja1@gmail.com>
2025-02-21 16:32:26 +0000

    Navigation using Tabs Navigation Patterns

---
+++ app/inbox.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const inbox = () => {
+  return (
+    <View>
+        <Text>Inbox Screen</Text>
+    </View>
+  )
+}
+
+export default inbox
---
+++ app/saved.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const saved = () => {
+  return (
+    <View>
+        <Text>Saved Screen</Text>
+    </View>
+  )
+}
+
+export default saved
